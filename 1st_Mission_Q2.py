e54ed477683c555ec269dc2d83d744d89ec8be14
leejw0709@gmail.com <leejw0709@gmail.com>
2022-01-16 19:19:51 +0900

    Create 1st_Mission_Q2.py

---
+++ 1st_Mission_Q2.py
@@ -0,0 +1,6 @@
+sentence = "way a is there will a is there where"
+
+def reverse_sentence(sentence):
+    return "".join(reversed(sentence))  # join 함수는 list를 문자열로 반환
+
+print(reverse_sentence(sentence))
