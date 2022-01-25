2847d255e2d41be50e3f9f521d3acf928a40a2c8
leejw0709@gmail.com <leejw0709@gmail.com>
2022-01-25 23:25:44 +0900

    Create 2nd_Mission_Q1.py

---
+++ 2nd_Week/2nd_Mission_Q1.py
@@ -0,0 +1,19 @@
+# test score, mid : 50, final : 75
+
+class Score():
+    # TODO
+    def __init__(self, mid, final):
+        self.__mid = mid
+        self.__final = final
+    
+    @property
+    def mid(self):
+        return self.__mid
+
+    @property
+    def final(self):
+        return self.__final
+        
+#출력함수
+score = Score(50, 75)
+print((score.mid + score.final) / 2)
