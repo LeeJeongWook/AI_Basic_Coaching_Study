9f9787c5c8d81969aba99ed7d187fafb59d34a4d
leejw0709@gmail.com <leejw0709@gmail.com>
2022-02-05 23:22:11 +0900

    Update 2nd_Mission_Q3.py

--- 2nd_Week/2nd_Mission_Q3.py
+++ 2nd_Week/2nd_Mission_Q3.py
@@ -1,5 +1,11 @@
 # 파일의 경로를 file_path로 설정
 # ex) file_path = ".data-01-test-score.csv"
 
+from asyncore import read
+
+
 def read_file(file_path):
     # TODO
+    return
+
+print(read_file(".data-01-test-score.csv"))
