a750552699fb4539eb5baf42fd0591c4bb18c1e0
LeeJeongWook <leejw0709@gmail.com>
2022-02-15 13:36:16 +0900

    Update 2nd_Week_Mission_Q3.py

--- 2nd_Week/Mission/2nd_Week_Mission_Q3.py
+++ 2nd_Week/Mission/2nd_Week_Mission_Q3.py
@@ -1,11 +1,18 @@
 # 파일의 경로를 file_path로 설정
 # ex) file_path = ".data-01-test-score.csv"
 
-from asyncore import read
+class ReadCSV(object):
+    def __init__(self, file_path):
+        self.file_path = file_path
 
+    def read_file(self):
+        with open(self.file_path) as file_data:
+            while True:
+                data = list(file_data.readline().split())
+                if not data: break
+                print(data)
 
-def read_file(file_path):
-    # TODO
-    return
+filepath = "..\Reference\data-01-test-score.csv"
+read_csv = ReadCSV(filepath)
 
-print(read_file(".data-01-test-score.csv"))
+print(read_csv.read_file())
