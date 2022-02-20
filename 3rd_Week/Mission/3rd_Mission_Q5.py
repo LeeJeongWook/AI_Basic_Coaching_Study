c7f7aebbffcabec9ff16274e86e6bd2386890c49
leejw0709@gmail.com <leejw0709@gmail.com>
2022-02-20 17:07:20 +0900

    Update 3rd_Mission_Q5.py

--- 3rd_Week/Mission/3rd_Mission_Q5.py
+++ 3rd_Week/Mission/3rd_Mission_Q5.py
@@ -1,11 +1,33 @@
+from asyncio import get_child_watcher
 import numpy as np
 import random
 
-arr1 = np.array([[5,7], [9,11]], float)
-arr2 = np.array([[2,4], [6,8]], float)
+xy = np.array([[1., 2., 3., 4., 5., 6.], [10., 20., 30., 40., 50., 60.]])
 
-concat_1 = np.concatenate((arr1, arr2), axis = 0)     # TODO axis 0
-concat_2 = np.concatenate((arr1, arr2), axis = 1)     # TODO axis 1
+x_train = xy[0]
+y_train = xy[1]
 
-print(concat_1)
-print(concat_2)
+# print(x_train, x_train.shape)
+# print(y_train, y_train.shape)
+# -------------------------3rd_Mission_Q3-------------------------
+
+beta_gd = np.random.rand(1)
+bias = np.random.rand(1)
+
+print(beta_gd, bias)
+# -------------------------3rd_Mission_Q4-------------------------
+
+learning_rate = 0.01
+
+for i in range(1000):
+    pred = (x_train * beta_gd) + bias
+    error = ((pred - y_train) ** 2).mean()
+
+    gd_w = ((pred - y_train) * 2 * x_train).mean()
+    gd_b = ((pred - y_train) * 2).mean()
+
+    beta_gd -= learning_rate * gd_w
+    bias -= learning_rate * gd_b
+
+    if i % 100 == 0:
+        print('Epoch({:10d} error: {:10f}, beta_gd: {:10f}, bias: {:10f}'.format(i, error, beta_gd.item(), bias.item()))
