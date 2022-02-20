acc9cf3d9d0140cd944c7775d1c186eabaa9fc1d
leejw0709@gmail.com <leejw0709@gmail.com>
2022-02-20 16:59:00 +0900

    Update 3rd_Mission_Q4.py

--- 3rd_Week/Mission/3rd_Mission_Q4.py
+++ 3rd_Week/Mission/3rd_Mission_Q4.py
@@ -7,5 +7,11 @@ arr2 = np.array([[2,4], [6,8]], float)
 concat_1 = np.concatenate((arr1, arr2), axis = 0)     # TODO axis 0
 concat_2 = np.concatenate((arr1, arr2), axis = 1)     # TODO axis 1
 
-print(concat_1)
-print(concat_2)
+# print(concat_1)
+# print(concat_2)
+# -------------------------3rd_Mission_Q3-------------------------
+
+beta_gd = np.random.rand(1)
+bias = np.random.rand(1)
+
+print(beta_gd, bias)
