5e662afbcae93036bc5672929f6929f11bb9a35b
LeeJeongWook <leejw0709@gmail.com>
2022-02-25 11:21:27 +0900

    Update 4th_Mission_Q1.py

---
+++ 4th_Week/Mission/4th_Mission_Q1.py
@@ -0,0 +1,11 @@
+import pandas as pd
+idx = ["HDD", "SSD", "USB", "CLOUD"]
+data = [19, 11, 5, 97]
+
+# 위 데이터로 Seires를 구현해보세요.
+series = pd.Series(data, index = idx)
+
+# 10 이상 20 이하를 가지는 데이터만 이용해 다시 Series를 정의하세요
+series = series[series >= 10][series <= 20]
+
+print(series)
